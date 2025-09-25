2099527136c1f61bab8562288283c3dd568973df
rafacoimbratec <102111979+rafacoimbratec@users.noreply.github.com>
2025-09-25 01:12:19 +0100

    Dynamic add in, no confirmation yet and no control

--- Thesis_Code/main.m
+++ Thesis_Code/main.m
@@ -1,14 +1,20 @@
 %% Planet Constants - Mars
 
 % === Mars Radius ===
-Mars_radius = 3389.5e3;  % [m]
+Mars_radius = 3396.2e3;  % [m]
 
 % === Mars Gravitational Parameter ===
 Mars_mu = 4.282837e13;  % Gravitational parameter for Mars [m^3/s^2]
 
+% Planetary spin rate [rad/s]
+Mars_omega = 7.088e-5;         % rad/s (sidereal rotation period ~24h 37m)
+
 % === Mars Atmospheric Parameters ===
-H_s = 11100;   % Scale height of Mars' atmosphere [m]
-rho_0 = 0.020; % Reference density of Mars' atmosphere at surface [kg/m^3]
+%H_s = 11100;   % Scale height of Mars' atmosphere [m]
+%rho_0 = 0.020; % Reference density of Mars' atmosphere at surface [kg/m^3]
+% Constants
+alpha1 = 559.35;
+alpha2 = 188.95;
 
 % === Specific Heat Ratio ===
 specific_heat = 1.29;  % Specific heat ratio for CO2-dominated Mars atmosphere (Cp/Cv), dimensionless
@@ -18,25 +24,33 @@ R_star = 8314.32;   % Universal gas constant [J/(kmol*K)]
 M_CO2 = 44.01;      % Molecular mass of CO2 [kg/kmol]
 R_s = R_star / M_CO2;  % Specific gas constant for Mars atmosphere [J/(kg*K)]
 
+% === Heat Flux Model ===
+N = 0.5;
+M = 3.15;
+k_heat_flux = 5.3697e-5;
+
+deg2rad = pi/180;
+
 %% Simulation Settings
 startTime = 0.0;  % Start time of simulation [s]
-stopTime = 1200;  % Stop time of simulation [s]
+stopTime = 7000;  % Stop time of simulation [s]
 stepTime = 0.01;  % Time step of simulation [s]
 
 %% Initial Conditions
 h0  = 125e3;        % Initial altitude [m]
-theta0 = -176.40167; % Initial longitude [deg]
-phi0   = -21.3;      % Initial latitude [deg]
+r0 = (h0+Mars_radius);
+theta0 = -176.40167*deg2rad; % Initial longitude [deg]
+phi0   = (-21.3)*deg2rad;      % Initial latitude [deg]
 V0  = 4700;          % Initial velocity [m/s]
-gamma0 = -10;        % Initial flight path angle [deg]
-psi0   = -2.8758;    % Initial heading angle [deg]
+gamma0 = (-10)*deg2rad;        % Initial flight path angle [deg]
+psi0   = (-2.8758)*deg2rad;    % Initial heading angle [deg]
 
 %% Final Conditions
-theta_t = -175.8;   % Target longitude [deg]
-phi_t   = 0.276;    % Target latitude [deg]
+theta_t = (-175.8)*deg2rad;   % Target longitude [deg]
+phi_t   = (0.276)*deg2rad;    % Target latitude [deg]
 V_t     = 450;      % Final velocity [m/s]
 h_t     = 2480;     % Final altitude [m]
-
+rt = (h_t+Mars_radius);
 %% Targeting Accuracy Requirement
 Rgo_target = 5e3;  % Maximum allowable targeting error [m] (5 km)
 
